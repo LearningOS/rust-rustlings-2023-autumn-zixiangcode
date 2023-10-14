a16a8f2426512eb49abd9a011ddb51c56397f7bd
zixiangcode <zixiangcode@gmail.com>
2023-10-14 19:17:41 +0800

    Finished options

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Point {
     x: i32,
@@ -13,8 +12,10 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
+    // match 语句的匹配选项 y 后面还要用到，但是 match 匹配时发生了移动语义
+    // 取得了所有权，所以可以加上 ref 使之只被借用而不被移动
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
