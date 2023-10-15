28e06302d8e989e7c787b41cb379471278d9dc24
zixiangcode <zixiangcode@gmail.com>
2023-10-15 19:34:22 +0800

    Finished traits

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -8,10 +8,11 @@
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+// 默认的 trait 实现，也就是 trait 定义处就实现了一个默认的 impl
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        String::from("Some information")
+    }
 }
 
 struct SomeSoftware {
