9da01904d84f12c21c58f1319807ed13cf9251c3
zixiangcode <zixiangcode@gmail.com>
2023-10-17 10:19:19 +0800

    Finished macros

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,16 +3,16 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// 不同的匹配用 ; 分隔 
 
 #[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
 
 fn main() {
